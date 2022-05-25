ade15a28d16d0b3c7657707e69993a68189c1a25
Donald Shahini <donald.shahini@norsk-tipping.no>
2022-05-25 18:08:58 +0200

    legger til dialog med QR code

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,6 +2,7 @@ function Card({ title, description, path }: CardProps) {
 	return (
 		<a
 			href={path}
+			target='_blank'
 			className='md:my-4 md:mx-4 mx-0 my-2 p-6 text-left	no-underline border border-gray-200	rounded-lg md:max-w-xs max-w-sm hover:text-blue-700 active:text-blue-700 focus:text-blue-700 hover:border-blue-700 active:border-blue-700 focus:border-blue-700'
 		>
 			<h2 className='mb-4 text-2xl'>{title} &rarr;</h2>
--- pages/home.tsx
+++ pages/home.tsx
@@ -101,23 +101,23 @@ const Home: NextPage = () => {
 					deployment to Vercel etc.
 				</p>
 
-				{/* <button
+				<button
 					className='py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'
 					onClick={() => setShowDialog(true)}
 				>
 					Show QR
-				</button> */}
+				</button>
 
 				<div className='flex items-center justify-center flex-wrap max-w-3xl'>
 					{cards.map((item) => (
 						<Card key={item.key} path={item.path} title={item.title} description={item.description} />
 					))}
 				</div>
-				{/* <Dialog show={showDialog} title='Lenke til demo' close={() => setShowDialog(!showDialog)}>
+				<Dialog show={showDialog} title='Lenke til demo' close={() => setShowDialog(!showDialog)}>
 					<div>
 						<QRCode value={`${origin}${router.pathname}`} level='H' size={340}/>
 					</div>
-				</Dialog> */}
+				</Dialog>
 			</main>
 
 			<footer className='flex flex-1 py-8 px-0 border-t justify-center items-center'>
